a011255ee06efe4c27887916c33a6896ebac3cf7
michal <michal.czuba.1995@gmail.com>
2019-10-10 23:55:14 +0200

    Initial commit

---
+++ src.py
@@ -0,0 +1,24 @@
+from datasets import Datasets
+
+path = '/Users/michal/PycharmProjects/MRP/datasets/*.tsp'
+data = Datasets(path)
+
+name = 'ali535'
+# name = 'berlin11_modified'
+# name = 'berlin52'
+# name = 'fl417'
+# name = 'gr666'
+# name = 'kroA100'
+# name = 'kroA150'
+# name = 'nrw1379'
+# name = 'pr2392'
+
+# print(data.datasets[name].loc[4]['x'])
+# print(data.distance_two_cities(name, 1, 2))
+# print(data.distance_permutation(name, [1, 2, 3, 4]))
+
+
+permutation = data.get_permutation(name)
+print(permutation)
+distance = data.distance_permutation(name, permutation)
+print(distance)
